79e274e8c89650778a19234c26f14f337a9f0beb
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-05-23 16:40:15 -0400

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,5 @@
 node('linux')
-{ 
+{
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/bashport.git'), string(name: 'PORT_DESCRIPTION', value: 'Bash is the GNU Projects the Bourne Again SHell' )]
   }
