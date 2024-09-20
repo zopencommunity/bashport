aabae132a507a767d87a94cd2bcce5b4976c5ec2
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:36:06 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,10 +6,10 @@ node('linux')
                       branches: [[name: '*/main']],
                       doGenerateSubmoduleConfigurations: false,
                       extensions: [],
-                      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/bashport.git']]])
+                      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/bashport.git']]])
         }
 
         stage('Build') {
-          build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/bashport.git'), string(name: 'PORT_DESCRIPTION', value: 'Bash is the GNU Projects the Bourne Again SHell' ),  string(name: 'BUILD_LINE', value: 'STABLE') ]
+          build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/bashport.git'), string(name: 'PORT_DESCRIPTION', value: 'Bash is the GNU Projects the Bourne Again SHell' ),  string(name: 'BUILD_LINE', value: 'STABLE') ]
         }
 }
